a7de4ee5b2df941ed423864777a1879179c8ae74
kunal394 <knlsingh394@gmail.com>
2016-03-18 02:18:22 +0530

    small changes

--- src/download.cpp
+++ src/download.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+string parsefilename(char url[])
+{
+	char *cname = strrchr(url, '/') + 1;
+	stringstream ss;
+	ss << cname;
+	return ss.str();
+}
+
 int main(int argc, char *argv[])
 {
 	if(argc < 2 ) {
@@ -11,7 +19,10 @@ int main(int argc, char *argv[])
 	string command;
 	stringstream ss;
 	ss << argv[1];
-	command = "wget " + ss.str() + " 1> /dev/null 2> /dev/null";
+	string filename;
+	filename = parsefilename(argv[1]);
+	command = "wget " + ss.str();
+	command += " 1> /dev/null 2> /dev/null -O " + filename;
 	if(system(command.c_str()))
 		printf("Could not execute command\n");
 	else
