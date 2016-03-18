86b38b530f71d2998c982221aabd6c32f5a56867
kunal394 <knlsingh394@gmail.com>
2016-03-18 09:05:09 +0530

    small changes

--- external/opm/install.cpp
+++ external/opm/install.cpp
@@ -34,15 +34,25 @@ string fetchconfurl(char *pname)
 	read_json(jsonFile, pt);
 
 	for(auto & package_list:pt){
-        if (package_list.second.get("packagename", "") == ss.str()){
-	    	for(auto & package:package_list.second){
-                if (package.first  == "conflink")
-                    return package.second.get_value<string>();
-	    	}
-        }
+		if (package_list.second.get("packagename", "") == ss.str()){
+			for(auto & package:package_list.second){
+				if (package.first  == "conflink")
+					return package.second.get_value<string>();
+			}
+		}
 	}
-	return "Could not find request package";
+	return "0";
 }
+
+/*int install_package(string fname)
+{
+	//return 1 on success, -1 otherwise
+}*/
+
+/*int update_local(char* pname)
+{
+	//return 1 on success, -1 otherwise
+}*/
 
 int main(int argc, char *argv[])
 {
@@ -60,13 +70,45 @@ int main(int argc, char *argv[])
 
 	//update the packages.list file
 	string s = "https://raw.githubusercontent.com/amoghbl1/OpenPackageManager/master/packages.list";
-	char* s_copy = (char*)alloca(s.size()+1);
-	memcpy(s_copy, s.c_str(), s.size()+1);
+	char* s_copy = (char*)alloca(s.size() + 1);
+	memcpy(s_copy, s.c_str(), s.size() + 1);
 	update(s_copy);
 
 	//fetch the url of the package.conf file
 	string confurl = fetchconfurl(argv[1]);
-	cout << "confurl: " << confurl << endl;
+	if(confurl.compare("0") == 0)
+	{
+		printf("Could not locate requested package. Installation failed\n");
+		return 0;
+	}
+	//cout << "conflink: " << confurl << endl;
+
+	//download the conf file
+	s_copy = (char*)alloca(confurl.size() + 1);
+	memcpy(s_copy, confurl.c_str(), confurl.size() + 1);
+
+	//get the conf file name and delete it in case of wrong download
+	string conf_fname;
+	conf_fname = parsefilename(s_copy);
+	//cout << "conf name: " << conf_fname << endl;
+
+	if(download(s_copy) == -1)
+	{
+		string command = "rm " + conf_fname;
+		system(command.c_str());
+		printf("Conf file could not be downloaded. Installation failed\n");
+		return 0;
+	}
+
+	//install package
+	/*if(install_package(conf_fname) == -1)
+	{
+		printf("Package installation failed\n");
+		return 0;
+	}*/
+
+	//update installed.list file
+	//while(update_local(argv[1]) != 1){}
 
 	return 0;
 }
