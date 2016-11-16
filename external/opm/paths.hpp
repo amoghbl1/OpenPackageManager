cac718cb280787a1c5eb14f7285f25bcf355e84c
Kunal Singh <knlsingh394@gmail.com>
2016-11-16 23:16:50 +0530

    added path for installed.list in paths.cpp

--- external/opm/paths.hpp
+++ external/opm/paths.hpp
@@ -1,6 +1,7 @@
 #define APP_BIN_PATH "/data/data/org.opm.openpackagemanager/app_bin/"
 #define APP_PACKAGES_PATH "/data/data/org.opm.openpackagemanager/app_packages/"
 #define PACKAGES_LIST_URL "http://web.iiit.ac.in/~amoghbl1/opm/packages.list"
+#define INSTALLED_LIST_PATH "./installed.list"
 
 //#define APP_BIN_PATH "/tmp/app_bin/"
 //#define APP_PACKAGES_PATH "/tmp/app_packages/"
